66fa7704e559d9fd21b89d17dd0388da0b66df3a
Guanzhou Zhao <benzhao2866@gmail.com>
2019-06-26 13:40:17 +1200

    preview uploading images

--- pages/vehicle/che.js
+++ pages/vehicle/che.js
@@ -100,6 +100,12 @@ Page({
     }
 
   },
+  previewUploadingImage: function(e) {
+    wx.previewImage({
+      current: e.currentTarget.id, // 当前显示图片的http链接
+      urls: this.data.files // 需要预览的图片http链接列表
+    })
+  },
   previewImage: function(e) {
     wx.previewImage({
       current: e.currentTarget.id, // 当前显示图片的http链接
